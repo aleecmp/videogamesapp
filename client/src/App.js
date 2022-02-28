0eeeeeae6e23b0dc1ac6343d4681916415ce5552
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-02-27 22:10:07 -0300

    Add LandingPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import "./App.css";
+import { Route, RouterBrowser } from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
+    <RouterBrowser>
+      <div className="App">
+        <Route exact path="/" component={LandingPage} />
+      </div>
+    </RouterBrowser>
   );
 }
 
