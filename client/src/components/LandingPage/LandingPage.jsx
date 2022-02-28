f4b449f82b4044c20ce8e0bfd3ccd246a22b44e5
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-02-27 22:12:25 -0300

    Add LandingPage

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <Link to="/videogames">
+        <button>Enter</button>
+      </Link>
+    </div>
+  );
 };
 
 export default LandingPage;
