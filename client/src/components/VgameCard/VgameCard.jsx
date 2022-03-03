56b8c87b47bf3772517f4c70768a6910f318fae0
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-03-02 23:01:01 -0300

    Add VgameCard component

---
+++ client/src/components/VgameCard/VgameCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./VgameCard.module.css";
+
+const VgameCard = (props) => {
+  console.log(props.name);
+  console.log(props.id);
+
+  return (
+    <div>
+      <Link to={`videogame/${props.id}`}>
+        <div>
+          <h1>{props.name.toUpperCase()}</h1>
+        </div>
+        <div>
+          <img className={styles.image} src={props.image} alt={props.name} />
+        </div>
+        <div>
+          {props.genres?.map((e) => (
+            <h5 key={e}>{e}</h5>
+          ))}
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default VgameCard;
