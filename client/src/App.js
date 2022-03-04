4f207c3acc5c1ffe3c13a6bce8d62a74c1a65303
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-03-03 21:27:03 -0300

    Add route creategame

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
-import { Route, RouterBrowser } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
+import Home from "./components/Home/Home";
+import VgameDetails from "./components/VgameDetails/VgameDetails";
+import CreateVgame from "./components/CreateVgame/CreateVgame";
 
 function App() {
   return (
-    <RouterBrowser>
+    <BrowserRouter>
       <div className="App">
         <Route exact path="/" component={LandingPage} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/videogame/:id" component={VgameDetails} />
+        <Route exact path="/create" component={CreateVgame} />
       </div>
-    </RouterBrowser>
+    </BrowserRouter>
   );
 }
 
