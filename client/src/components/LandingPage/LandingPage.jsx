c7198c73079271241e77dfff7477f01d749a07f5
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-03-04 21:54:14 -0300

    Add LandingPage component

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
   return (
     <div>
       <h1>Landing Page</h1>
-      <Link to="/videogames">
+      <Link to="/home">
         <button>Enter</button>
       </Link>
     </div>
