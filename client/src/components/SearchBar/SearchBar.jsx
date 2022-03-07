4ac51a1506ebc3f492e431558143deaa2aedd4a7
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-03-06 22:25:55 -0300

    Add loading

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -7,38 +7,67 @@ import Loading from "../Loading/Loading";
 const SearchBar = () => {
   const dispatch = useDispatch();
   const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleInputChange = (e) => {
     e.preventDefault();
     setName(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch(getVgameByName(name));
+    setLoading(true);
+    try {
+      await dispatch(getVgameByName(name));
+      setLoading(false);
+    } catch (e) {
+      console.log("Handle errors here");
+    }
     setName("");
   };
 
   return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          handleSubmit(e);
-        }}
-      >
-        <input
-          type="text"
-          name="name"
-          value={name}
-          placeholder="Search videogame"
-          onChange={(e) => {
-            handleInputChange(e);
-          }}
-        />
-        {name ? <button type="submit">Search</button> : <button>Search</button>}
-      </form>
-    </div>
+    <form
+      onSubmit={(e) => {
+        handleSubmit(e);
+      }}
+    >
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={handleInputChange}
+            placeholder="Search for a Vgame"
+          />
+          <button>Search</button>
+        </div>
+      )}
+    </form>
   );
 };
+
+//     <div>
+//       <form
+//         onSubmit={(e) => {
+//           handleSubmit(e);
+//         }}
+//       >
+//         <input
+//           type="text"
+//           name="name"
+//           value={name}
+//           placeholder="Search videogame"
+//           onChange={(e) => {
+//             handleInputChange(e);
+//           }}
+//         />
+//         {name ? <button type="submit">Search</button> : <button>Search</button>}
+//       </form>
+//     </div>
+//   );
+// };
 
 export default SearchBar;
