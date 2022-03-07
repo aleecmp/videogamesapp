88f97bf26d4f9b46fe536bfae238ecbb05d886d8
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-03-07 16:27:46 -0300

    Add loading feature

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 import { useDispatch } from "react-redux";
 import { getAllVgames } from "../../redux/actions";
+import Loading from "../Loading/Loading";
 
 const NavBar = () => {
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
 
-  let handleClick = (e) => {
+  const handleClick = async (e) => {
     e.preventDefault();
-    dispatch(getAllVgames());
+    setLoading(true);
+    try {
+      await dispatch(getAllVgames());
+      setLoading(false);
+    } catch (e) {
+      console.log("Handle errors here");
+    }
   };
 
   return (
@@ -17,18 +25,19 @@ const NavBar = () => {
       <Link to="/">
         <button>Landing Page</button>
       </Link>
-      <button
-        onClick={(e) => {
-          handleClick(e);
-        }}
-      >
-        Reset
-      </button>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <button onClick={handleClick}>Clean filters</button>
+        </div>
+      )}
 
-      <SearchBar />
       <Link to="/create">
         <button>Create Vgame</button>
       </Link>
+
+      <SearchBar />
     </div>
   );
 };
