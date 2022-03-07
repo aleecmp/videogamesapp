813eecd6003ba09bd1ad6cdd20f843bd0e574d3c
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-03-07 16:26:59 -0300

    Add buttons prev-next

--- client/src/components/Paginated/Paginated.jsx
+++ client/src/components/Paginated/Paginated.jsx
@@ -1,21 +1,52 @@
 import React from "react";
 
-const Paginated = ({ vgamesPerPage, allVgames, paginated }) => {
+const Paginated = ({
+  vgamesPerPage,
+  allVgames,
+  paginated,
+  setCurrentPage,
+  currentPage,
+}) => {
   const pageNumbers = [];
 
   for (let i = 0; i < Math.ceil(allVgames / vgamesPerPage); i++) {
     pageNumbers.push(i + 1);
   }
   return (
-    <nav>
-      <div>
-        {pageNumbers?.map((number) => (
-          <div key={number}>
-            <button onClick={() => paginated(number)}>{number}</button>
-          </div>
-        ))}
-      </div>
-    </nav>
+    <ul>
+      <button
+        onClick={() =>
+          setCurrentPage(currentPage === 1 ? currentPage : currentPage - 1)
+        }
+      >
+        Prev
+      </button>
+      {pageNumbers?.map((number) => (
+        <button key={number} onClick={() => paginated(number)}>
+          {number}
+        </button>
+      ))}
+      <button
+        onClick={() =>
+          setCurrentPage(
+            currentPage === allVgames ? currentPage : currentPage + 1
+          )
+        }
+      >
+        {" "}
+        Next{" "}
+      </button>
+    </ul>
+
+    // <nav>
+    //   <div>
+    //     {pageNumbers?.map((number) => (
+    //       <div key={number}>
+    //         <button onClick={() => paginated(number)}>{number}</button>
+    //       </div>
+    //     ))}
+    //   </div>
+    // </nav>
   );
 };
 
