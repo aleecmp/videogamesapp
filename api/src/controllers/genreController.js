e2d822202e6e090a30debd9af4fcea1096f638eb
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-03-10 12:48:05 -0300

    Add fix getAllGenres

--- api/src/controllers/genreController.js
+++ api/src/controllers/genreController.js
@@ -7,24 +7,20 @@ const getAllGenres = async () => {
     let genre = await axios.get(
       `https://api.rawg.io/api/genres?key=${API_KEY}`
     );
-    genre = genre.data.results.map((g) => {
-      return {
-        id: g.id,
-        name: g.name,
-      };
-    });
-    console.log(genre);
+
+    genre = genre.data.results;
+    // console.log(genre);
     console.log(genre.length);
     genre.forEach((e) => {
       Genre.findOrCreate({
         where: {
-          id: e.id,
           name: e.name,
         },
       });
     });
 
     const dbGenre = await Genre.findAll();
+
     return dbGenre;
   } catch (err) {
     console.log(err);
