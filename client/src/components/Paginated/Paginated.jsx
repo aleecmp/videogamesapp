45d9c82bf9ecdfbb5f96879189b6a2897eb0257c
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-03-11 21:28:14 -0300

    Add upgrade prev-next

--- client/src/components/Paginated/Paginated.jsx
+++ client/src/components/Paginated/Paginated.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Paginated.module.css";
 
 const Paginated = ({
   vgamesPerPage,
@@ -13,40 +14,37 @@ const Paginated = ({
     pageNumbers.push(i + 1);
   }
   return (
-    <ul>
-      <button
-        onClick={() =>
-          setCurrentPage(currentPage === 1 ? currentPage : currentPage - 1)
-        }
-      >
-        Prev
-      </button>
-      {pageNumbers?.map((number) => (
-        <button key={number} onClick={() => paginated(number)}>
-          {number}
+    <nav className={styles.container}>
+      <ul className={styles.paginated}>
+        <button
+          onClick={() =>
+            setCurrentPage(currentPage === 1 ? currentPage : currentPage - 1)
+          }
+        >
+          {" "}
+          &#60;{" "}
         </button>
-      ))}
-      <button
-        onClick={() =>
-          setCurrentPage(
-            currentPage === allVgames ? currentPage : currentPage + 1
-          )
-        }
-      >
-        {" "}
-        Next{" "}
-      </button>
-    </ul>
-
-    // <nav>
-    //   <div>
-    //     {pageNumbers?.map((number) => (
-    //       <div key={number}>
-    //         <button onClick={() => paginated(number)}>{number}</button>
-    //       </div>
-    //     ))}
-    //   </div>
-    // </nav>
+        {pageNumbers?.map((number) => (
+          <button
+            className={styles.button}
+            key={number}
+            onClick={() => paginated(number)}
+          >
+            {number}
+          </button>
+        ))}
+        <button
+          onClick={() =>
+            setCurrentPage(
+              currentPage === allVgames ? currentPage : currentPage + 1
+            )
+          }
+        >
+          {" "}
+          &#62;{" "}
+        </button>
+      </ul>
+    </nav>
   );
 };
 
