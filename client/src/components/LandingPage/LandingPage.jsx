6d591981e6a606b80cabff02a70099e83c1c1a78
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-03-13 22:09:55 -0300

    Add css

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./LandingPage.module.css";
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Welcome to AppVgames</h1>
+      <h3 className={styles.subtitle}>
+        Check the information of your favorite videogames
+      </h3>
       <Link to="/home">
-        <button>Enter</button>
+        <button className={styles.button}>Enter</button>
       </Link>
     </div>
   );
