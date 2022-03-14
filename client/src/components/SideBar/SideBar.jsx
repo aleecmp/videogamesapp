358005762ecd0cdea9842180ac3b266cab9e1939
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-03-13 22:11:27 -0300

    Add changes navbar

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import GenresFilter from "../Filters/GenresFilter";
 import OriginFilter from "../Filters/OriginFilter";
-import OrderAlph from "../Order/OrderAlph";
-import OrderRating from "../Order/OrderRating";
+import OrderBy from "../OrderBy/OrderBy";
+import Loading from "../Loading/Loading";
+import { getAllVgames } from "../../redux/actions";
+import styles from "./SideBar.module.css";
 
 const SideBar = () => {
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      await dispatch(getAllVgames());
+      setLoading(false);
+    } catch (e) {
+      console.log("Handle errors here");
+    }
+  };
+
   return (
-    <div>
-      <h3>Filters</h3>
-      <GenresFilter />
-      <OriginFilter />
-      <h3>Order</h3>
-      <OrderAlph />
-      <OrderRating />
+    <div className={styles.container}>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="sidebar">
+          <div className="sidebar__filter">
+            <h3>Filters</h3>
+            <GenresFilter />
+            <OriginFilter />
+
+            <OrderBy />
+            <button onClick={handleClick}>Clean</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
